fec4bf76c3c1694b778b9549ec8cda0d0e5a1fd0
Rodrigo Ribes <104105156+rodri-ribes@users.noreply.github.com>
2022-10-01 18:13:11 -0300

    first commit

--- api/src/app.js
+++ api/src/app.js
@@ -12,18 +12,11 @@ const server = express();
 server.name = 'API';
 server.use(cors());
 
-server.use(bodyParser.urlencoded({ extended: true}));
+server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 server.use(cookieParser());
 server.use(morgan('dev'));
 
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
 
 server.use('/', routes);
 
--- client/src/components/Countrys/Countrys.js
+++ client/src/components/Countrys/Countrys.js
@@ -4,79 +4,87 @@ import { useSelector } from "react-redux";
 import { CardCountry } from "../CardCountry/CardCountry";
 import NotFound from '../functions/NotFound/NotFound';
 import { useEffect, useState } from 'react';
+import Spinner from '../../Spinner/Spinner'
+
 
 export default function Countrys() {
-    let countrysAll = useSelector(state => state.countrys);
-    
-    let error = useSelector(state => state.error);
+  let countrysAll = useSelector(state => state.countrys);
 
-    const [paginado, setPaginado] = useState(0);
-    
+  let error = useSelector(state => state.error);
 
-    let nextPage = () => {
-      if (countrysAll.length <= paginado + 10) {
-        setPaginado(paginado);
-      } else setPaginado(paginado + 10);
-    };
+  const [paginado, setPaginado] = useState(0);
 
-    let prevPage = () => {
-      if (paginado < 9) {
-        setPaginado(0);
-      } else {
-        setPaginado(paginado - 10);
-      }
-    };
-  
-    const firstPage = () => {
+
+  let nextPage = () => {
+    if (countrysAll.length <= paginado + 10) {
+      setPaginado(paginado);
+    } else setPaginado(paginado + 10);
+  };
+
+  let prevPage = () => {
+    if (paginado < 9) {
       setPaginado(0);
-    };
-  
-    const lastPage = () => {
-      setPaginado(countrysAll.length - 10);
-    };
-  
-    useEffect(() => {
-      firstPage()
-    }, [countrysAll]);
-
-    let filteredC;
-
-    if(paginado === 0){
-        filteredC = countrysAll.slice(paginado, paginado + 9);
-    }else{
-        filteredC = countrysAll.slice(paginado, paginado + 10);
+    } else {
+      setPaginado(paginado - 10);
     }
+  };
 
-    return(
+  const firstPage = () => {
+    setPaginado(0);
+  };
+
+  const lastPage = () => {
+    setPaginado(countrysAll.length - 10);
+  };
+
+  useEffect(() => {
+    firstPage()
+  }, [countrysAll]);
+
+  let filteredC;
+
+  if (paginado === 0) {
+    filteredC = countrysAll.slice(paginado, paginado + 9);
+  } else {
+    filteredC = countrysAll.slice(paginado, paginado + 10);
+  }
+
+  return (
+    <>
+      {countrysAll ?
         <>
-            <div className={style.Container}>
-                {error ? 
-                  <div className={style.ContainerNotFound}>
-                    <NotFound text="Country Not Found"/> 
-                  </div>
-                  :
-                    filteredC.map(country => {
-                        return(
-                            <CardCountry
-                            img={country.flag}
-                            name={country.name}
-                            continent={country.continent}
-                            key={country.id}
-                            />
-                            )
-                        })
-                    }
-            </div>
-            {error?
-              null
-            :
-              <div className={style.Container__btns}>
-                  <button onClick={firstPage}>{'<<'}</button>
-                  <button onClick={prevPage}>{'<'}</button>
-                  <button onClick={nextPage}>{'>'}</button>
-                  <button onClick={lastPage}>{'>>'}</button>
+          <div className={style.Container}>
+            {error ?
+              <div className={style.ContainerNotFound}>
+                <NotFound text="Country Not Found" />
               </div>
+              :
+              filteredC.map(country => {
+                return (
+                  <CardCountry
+                    img={country.flag}
+                    name={country.name}
+                    continent={country.continent}
+                    key={country.id}
+                  />
+                )
+              })
             }
+          </div>
+          {error ?
+            null
+            :
+            <div className={style.Container__btns}>
+              <button onClick={firstPage}>{'<<'}</button>
+              <button onClick={prevPage}>{'<'}</button>
+              <button onClick={nextPage}>{'>'}</button>
+              <button onClick={lastPage}>{'>>'}</button>
+            </div>
+          }
         </>
-    )
+        :
+        <Spinner />
+      }
+    </>
+  )
 }
