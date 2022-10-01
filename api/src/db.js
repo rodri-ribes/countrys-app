cfbb2c97e9edd6a4556e701e9177ef26d8848b57
Rodrigo Ribes <104105156+rodri-ribes@users.noreply.github.com>
2022-10-01 17:35:00 -0300

    first commit

--- api/src/db.js
+++ api/src/db.js
@@ -1,17 +1,11 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const pg = require('ng');
+const pg = require('pg');
 
-const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_LINK,
-} = process.env;
-
-
-const sequelize = new Sequelize(DB_LINK, {
+const sequelize = new Sequelize(process.env.DB_LINK, {
   logging: false,
+  native: false,
   dialectModule: pg
 });
-
-
 
 module.exports = sequelize;
